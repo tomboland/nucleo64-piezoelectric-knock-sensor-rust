0e488fe56a167e87fdb87898198d3e7431b53a7b
Tom Boland <github@t0mb.net>
2022-11-10 09:29:26 +0000

    Adding cargo config, and cleaning up

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use cortex_m_semihosting::hprintln;
 // pick a panicking behavior
 use crate::hal::delay::Delay;
 use crate::hal::{
-    adc::{config::AdcConfig, config::SampleTime, Adc},
+    adc::{config::AdcConfig, config::Resolution, config::SampleTime, Adc},
     pac,
     prelude::*,
 };
 use cortex_m_rt::entry;
 use panic_halt as _;
 use stm32f4xx_hal as hal;
+
+const ADC_RESOLUTION: Resolution = Resolution::Twelve;
+const ADC_RESOLUTION_BITS: u32 = 12;
+const REFERENCE_VOLTAGE: f64 = 3.3;
+const KNOCK_THRESHOLD_VOLTAGE: f64 = 1.;
 
 #[entry]
 fn main() -> ! {
@@ -20,7 +25,11 @@ fn main() -> ! {
     ) {
         let gpioa = dp.GPIOA.split();
         let pa0 = gpioa.pa0.into_analog();
-        let mut adc = Adc::adc1(dp.ADC1, true, AdcConfig::default());
+        let mut adc = Adc::adc1(
+            dp.ADC1,
+            true,
+            AdcConfig::default().resolution(ADC_RESOLUTION),
+        );
 
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
@@ -29,7 +38,9 @@ fn main() -> ! {
 
         loop {
             let sample = adc.convert(&pa0, SampleTime::Cycles_480);
-            if (sample as f64 / 4096.) * 5. > 1. {
+            if (sample as f64 / (2 as u32).pow(ADC_RESOLUTION_BITS) as f64) * REFERENCE_VOLTAGE
+                > KNOCK_THRESHOLD_VOLTAGE
+            {
                 hprintln!("Knock!").unwrap();
             }
             delay.delay_ms(10_u32);
